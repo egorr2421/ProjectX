c77f4510e9382fe0640ae3e5dd46579c86699172
cavenoji <ickpanah@gmail.com>
2018-12-09 19:58:31 +0200

    Add migrations tool, add base for login/registration

--- repository/UsersRepository.js
+++ repository/UsersRepository.js
@@ -1,11 +1,28 @@
 'use strict'
 
 const User = require('../model/User');
+const Op = require('sequelize').Op;
 
-module.exports.method = function findAll() {
-    return User.findAll
+const findAll = User.findAll;
+const findById = User.findById;
+const findByEmailOrLogin = (value) => User.findAll({
+    where: {
+        [Op.or]: [{email: value.toLowerCase()}, {login: value.toLowerCase()}]
+    }
+});
+const insert = (login, password, avatar, email) => {
+    User.build({login: login, password: password, avatar: avatar, email: email})
+        .save()
+        .then((user) => user.tokenId)
+        .catch((error) => {
+            console.error(error);
+        }
+    );
 }
 
-module.exports.method = function findById(userId) {
-    User.findById(userId);
-}
+module.exports.findAll = findAll;
+module.exports.findById = findById;
+module.exports.findByEmailOrLogin = findByEmailOrLogin;
+module.exports.insert = insert;
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,59 @@
+'use strict'
+
 const UserRepository = require('../repository/UsersRepository');
+const ResponseStatus = require('./response/ResponseStatus')
 var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('main/index.ejs', {title: "Main"});
+  res.render('main/index.ejs', {title: "Main"});
 });
 
 router.get('/login', function (req, res, next) {
-    console.info(UserRepository.findAll);
-    res.render('login/index.ejs', {title: "login"});
-});
-router.post('/login', function (req, res, next) {
-    res.render('login/index.ejs', {title: "login"});
+  res.render('login/index.ejs', {title: "login"});
 });
 
+router.post('/login', function (req, res, next) {
+  const user = UserRepository.findByEmailOrLogin(req.body.email
+      || req.body.login);
+  if (user.password !== req.body.password) {
+    res.status(ResponseStatus.UNATHORIZED);
+    //TODO: res.render(error)
+  }
+  res.render('login/index.ejs', {title: "login"});
+});
 
 router.get('/register', function (req, res, next) {
-    res.render('register/index.ejs', {title: "register"});
+  res.render('register/index.ejs', {title: "register"});
 });
 
 router.post('/register', function (req, res, next) {
-    pool.query('INSERT INTO users(login,pass,avatar,email)' +
-        ' values($1,$2,$3,$4)', [req.body.login, req.body.pass, 1, req.body.email],
-        function (err, res) {
-        console.log(err);
-        console.log(res);
-    });
+  console.info(req.body);
+  UserRepository.findByEmailOrLogin(req.body.email
+      || req.body.login).then(value => {
+        if(value == null || value.length == null || value.length == 0) {
+          const sessionId = UserRepository.insert(req.body.email.toLowerCase(),
+              req.body.pass, req.body.avatar || "", req.body.login.toLowerCase());
+          createCookie(res, sessionId);
+          res.status(ResponseStatus.CREATED);
+        } else {
+          res.status(ResponseStatus.ERROR);
+        }
     res.render('register/index.ejs', {title: "register"});
+  }).catch(error => console.error(error));
 });
-
 
 router.get('/:id', function (req, res, next) {
-    res.render('main/index.ejs', {title: "Main"});
+  res.render('main/index.ejs', {title: "Main"});
 });
+
+const createCookie = (res, sessionId) => {
+  res.cookie('session_id', sessionId, {
+    maxAge: 1000 * 60 * 60 * 24 * 30,
+    httpOnly: true,
+    // signed: true
+  });
+}
+
 module.exports = router;
